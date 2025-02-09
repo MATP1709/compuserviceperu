9914b5f83ef08f04a391b5efa8ce1965454910af
MATP1709 <102614204+MATP1709@users.noreply.github.com>
2025-02-08 21:23:28 -0500

    cambios para subir web

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://matp1709.github.io/compuserviceperu/',
+  base: '/compuserviceperu/',
   plugins: [react()],
 })
